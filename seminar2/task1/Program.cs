e11dabb9299a19a4998985140f669b71358a9f10
ivan Mokeev <mig000085@gmail.com>
2024-03-20 19:11:13 +0400

    seminar2Task1

--- seminar2/task1/Program.cs
+++ seminar2/task1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задание 1. Совместная работа
+Семинар 2. Простые алгоритмы на C#
+Напишите программу, которая принимает на
+вход трёхзначное число и удаляет вторую
+цифру этого числа.
+Примеры
+a = 256 => 26
+a = 891 => 81*/
+
+int a = 891;
+int b = (a / 100);
+int c = (a % 10);
+Console.WriteLine($"{b}{c}");
+Console.WriteLine($"[{b}, {c}]");
+
+string name = "Vasia";
+int age = 10;
+Console.WriteLine($"Hello {name}! Your age is {age}");
+Console.WriteLine(age + " ");
