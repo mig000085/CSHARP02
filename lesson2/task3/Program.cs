24f1d5103b0317843c10de87125c2e46a504b1e0
ivan Mokeev <mig000085@gmail.com>
2024-03-20 19:38:40 +0400

    .\lesson2Task3

---
+++ lesson2/task3/Program.cs
@@ -0,0 +1,25 @@
+﻿//int n = 10;
+//int[] array = { 2, 6, 4, 7, 8, 36, 1, 3, 8, 5 };
+//int i = 0;
+//
+//while (i < n)
+//{
+//    if(array[i] % 2 == 0)
+//    {
+//            Console.Write($"{array[i]} ");
+//    }
+//    i = i + 1;
+//}
+
+//int m = 10;
+int[] array1 = { 10, 21, 31, 51, 61, 71, 81, 91, 61, 31 };
+int j = 0;
+
+while (j < array1.Length)
+{
+    if (array1[j] % 2 == 0)
+    {
+        Console.Write($"{array1[j]} ");
+    }
+    j = j + 1;
+}
