794ad4713b716d60b049d55ef0a9bd2f407eed13
ivan Mokeev <mig000085@gmail.com>
2024-03-20 19:12:42 +0400

    seminar1

--- seminar1/Homework1/Program.cs
+++ seminar1/Homework1/Program.cs
@@ -3,15 +3,16 @@
 //какое число большее, а какое меньшее.
 using System;
 
-public class Answer {
+public class Answer
+{
     static void CompareNumbers(int firstNumber, int secondNumber)
     {
         // Введите свое решение ниже
-        if(firstNumber > secondNumber)
+        if (firstNumber > secondNumber)
         {
             System.Console.WriteLine("первое число больше");
         }
-        else if(firstNumber < secondNumber)
+        else if (firstNumber < secondNumber)
         {
             System.Console.WriteLine("второое число больше");
         }
@@ -20,15 +21,19 @@ public class Answer {
     }
 
 
-  // Не удаляйте и не меняйте метод Main! 
-    static public void Main(string[] args) {
+    // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args)
+    {
         int firstNumber, secondNumber;
 
-        if (args.Length >= 2) {
+        if (args.Length >= 2)
+        {
             firstNumber = int.Parse(args[0]);
             secondNumber = int.Parse(args[1]);
-        } else {
-           // Здесь вы можете поменять значения для отправки кода на Выполнение
+        }
+        else
+        {
+            // Здесь вы можете поменять значения для отправки кода на Выполнение
             firstNumber = 55;
             secondNumber = 10;
         }
