73d1381f5a1d6676a0f0776128cf83888102bae8
ivan Mokeev <mig000085@gmail.com>
2024-03-24 19:14:26 +0400

    .\seminar3Task1

---
+++ seminar3/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Примеры создания массивов в C#
+
+// int [] array = new int [4]; выделяется память на 4 элемента, заполнен нулями.
+// int [] array = new int [4] { 5, 6, 7, 8 }; заполнен указанными элементами.
+// int [] array = new int [] { 5, 6, 7, 8 }; заполнен указанными элементами.
+// int[] array = { 5, 6, 7, 8 };
+
+
+
+int[] array = { 5, 6, 7, 8 };
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(i + " : " + array[i]);
+}
+Console.WriteLine();
+
+foreach (var item in array)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine();
