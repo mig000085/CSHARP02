9b4a69bc97e2e27e411c4b2410eb676464f85845
ivan Mokeev <mig00085@gmail.com>
2024-03-25 22:17:15 +0400

    .\seminar4Task1

--- seminar4/Task1/Program.cs
+++ seminar4/Task1/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿// Задание 1. Совместная работа
+// Семинар 4. Функции
+// 20 мин
+// Задайте одномерный массив, заполненный случайными
+// числами. Определите количество простых чисел в этом
+// массиве.
+// Примеры
+// [1 3 4 19 3] => 2
+// [4 3 4 1 9 5 21 13] => 3
 
-namespace Task1
+// Console.WriteLine("hello!");
+// string str = "2024";
+// int num = Convent.ToInt32("str");
+
+int[] CreateArrRnaInt(int size, int min, int max)
 {
-    class Program
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+        array[i] = rnd.Next(min, max)
+ 
+    
+
+
+    return array;
 }
