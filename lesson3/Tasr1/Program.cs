b21508ff31fe15a30e974d30864a32aa3d1f4349
ivan Mokeev <mig000085@gmail.com>
2024-03-25 15:13:13 +0400

    lesson3task1

---
+++ lesson3/Tasr1/Program.cs
@@ -0,0 +1,12 @@
+﻿double CalculateFormula(int a, int b, int c, int d);
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+
+double result = calculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
+    }
+}
