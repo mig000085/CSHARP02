3b1105467f7992bf2cd8355ae297ef0a35136ff2
mig000085 <160721421+mig000085@users.noreply.github.com>
2024-03-30 19:22:45 +0400

    .\less4Task1

--- lesson4/task1/Program.cs
+++ lesson4/task1/Program.cs
@@ -13,7 +13,39 @@ for (int i = 0; i < matrix.GetLength(1); i++)
 {
     for (int j = 0; j < matrix.GetLength(0); j++)
     {
+
         Console.Wrireline($"{matrix[i, j]} ");
     }
     Console.Wrireline();
 }
+
+int[,] CreateMatrix(int rowCout, int columsCout)
+{
+    int[,] matrix = new int[int rowCout, int columsCout];
+
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+
+            Console.Wrireline($"{matrix[i, j]} ");
+        }
+        Console.Wrireline();
+    }
+}
+
+int[,] matrix = CreateMatrix(4, 5);
+ShowMatrix(matrix);
