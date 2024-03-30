83c3044ec831ff5d5f5f4e4cd08a8e67b350f3b5
mig000085 <160721421+mig000085@users.noreply.github.com>
2024-03-30 19:01:05 +0400

    .\less4Task1

--- lesson4/task1/Program.cs
+++ lesson4/task1/Program.cs
@@ -1,20 +1,19 @@
-﻿int|,| matrix = new int|3, 3|; // Example size: 3x3 matrix
+﻿int[,] matrix = new int[3, 5];
 
 Random rand = new Random();
 for (int i = 0; i < matrix.GetLength(0); i++)
 {
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        matrix|i, j| = rand.Next(1, 11);
+        matrix[i, j] = rand.Next(1, 11);
     }
 }
 
-for (int i = 0; i < matrix.GetLength(0); i++)
+for (int i = 0; i < matrix.GetLength(1); i++)
 {
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    for (int j = 0; j < matrix.GetLength(0); j++)
     {
-        // Interpolation of strings
-        Console.Write($"{matrix|i, j|} ");
+        Console.Wrireline($"{matrix[i, j]} ");
     }
-    Console.WriteLine(); // Move to the next line after each row
+    Console.Wrireline();
 }
