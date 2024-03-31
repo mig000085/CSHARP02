8794cee434ac6788dafa7b536de38389ac8bd850
mig000085 <160721421+mig000085@users.noreply.github.com>
2024-03-31 17:15:22 +0400

    .\lesson4Task1

--- lesson4/task2/Program.cs
+++ lesson4/task2/Program.cs
@@ -13,8 +13,21 @@
     return matrix;
 }
 
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
 
 int[,] matrix = CreateMatrix(3, 4);
+ShowMatrix(matrix);
 foreach (int e in matrix)
 {
     if (IsInteresting(e) == true)
