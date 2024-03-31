356c82ba1e5fb53b604036fe91451748f9548d87
mig000085 <160721421+mig000085@users.noreply.github.com>
2024-03-31 18:50:40 +0400

    .\sem6task1

---
+++ seminar6/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте массив символов (тип char []). Создайте строку из
+// символов этого массива.
+// Указание
+// Конструктор строки вида string(char []) не использовать.
+//[‘a’, ‘b’, ‘c’, ‘d’] => “abcd”
+
+char[] chars = { 'a', 'b,' 'c', 'd' };
+string strResuit = string.Empty;
+
+for (int i = 0; i < chars.Length; i++)
+{
+    strResuit += chars[i]; // c#
+    //strResuit = strResuit + chars[i]; jaua
+}
+Console.whileLine(strResuit);
