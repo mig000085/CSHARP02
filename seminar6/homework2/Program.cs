dc590d12183cbde00305819cf29cb9a04c58b082
mig000085 <160721421+mig000085@users.noreply.github.com>
2024-04-01 16:22:23 +0400

    .\seminar6Hom2

---
+++ seminar6/homework2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы
+// в обоих регистрах. Сформируйте строку, в которой все
+// заглавные буквы заменены на строчные. 
+
+// Console.WriteLine(str);
+// string result;
+
+string StringUpperToLower(string str)
+{
+    string result = string.Empty;
+    for (int i = 0; i < str.Length; i++)
+    {
+        int value = (int)(str[i]);
+        if (value >= 65 && value <= 90)
+        {
+            result = result + (char)(value + 32);
+        }
+        else
+        {
+            result = result + str[i];
+        }
+    }
+    return result;
+}
+
+Console.WriteLine("Введите строку=");
+
+string str = Console.ReadLine();
+
+string result = StringUpperToLower(str);
+
+Console.WriteLine(result);
