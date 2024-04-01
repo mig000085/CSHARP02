2475f8c41fbf44c5ec9e61be4c7edf2a7ab8a0f0
mig000085 <160721421+mig000085@users.noreply.github.com>
2024-04-01 16:25:21 +0400

    .\seminar6Hom3

---
+++ seminar6/homework3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните,
+// является ли она палиндромом.
+
+
+bool IsStringPalindrome(string str)
+{
+    for (int i = 0; i < str.Length / 2; i++)
+    {
+        if (str[i] != str[str.Length - i - 1])
+            return false;
+    }
+    return true;
+
+}
+
+Console.WriteLine("Введите строку палиндром");
+string str = Console.ReadLine();
+
+Console.WriteLine(IsStringPalindrome(str) ? "Строка палиндром" : "Строка не палиндром");
