37c613f650cadd315e03df466d018b4a5f2926f6
mig000085 <160721421+mig000085@users.noreply.github.com>
2024-04-02 15:33:59 +0400

    .\lesson1task1ODJ

--- lesson1/task.1/obj/Debug/net8.0/task.1.AssemblyInfo.cs
+++ lesson1/task.1/obj/Debug/net8.0/task.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01ded16b829e41c527fd3362d2eacbeb3c55b06b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bc137883a3d413d82ef83725a952e03abd8353f")]
 [assembly: System.Reflection.AssemblyProductAttribute("task.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
