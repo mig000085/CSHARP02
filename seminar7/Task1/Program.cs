ff7317ebc452e33a4c3cccf0ab177ab296d3169f
mig000085 <160721421+mig000085@users.noreply.github.com>
2024-04-06 15:41:36 +0400

    seminar7Task1

---
+++ seminar7/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// N=5 => 1 2 3 4 5*/
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+PrintNaturalNumbers(number);
+
+void PrintNaturalNumbers(int num)
+{
+    if (num == 0) return;
+    PrintNaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
+
+
+
+
+
