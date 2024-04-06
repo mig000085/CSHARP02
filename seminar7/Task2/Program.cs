31b59c77cf9a004819cc9173dfc8d87c9b71e065
mig000085 <160721421+mig000085@users.noreply.github.com>
2024-04-06 16:09:04 +0400

    seminar7Task2

---
+++ seminar7/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// Указание
+// Использовать рекурсию.
+// Пример
+// 123 => 6
+// 63 => 9
+
+int SumDigit(int number) // 123 12 1 0
+{
+    if (number == 0) return 0; // Pass 0
+    return number % 10 + SumDigit(number / 10); // 123 12 1
+    // 1%10 + 12%10 + 123%10 + 0 = 6
+}
+
+int number = 123;
+int sum = SumDigit(number);
+System.Console.WriteLine(sum);
