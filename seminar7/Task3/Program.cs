7d0519fbc40971665364631fff2093bed2ec3263
mig000085 <160721421+mig000085@users.noreply.github.com>
2024-04-06 17:12:05 +0400

    seminar7Task3

---
+++ seminar7/Task3/Program.cs
@@ -0,0 +1,45 @@
+﻿
+// Считать строку с консоли, содержащую латинские буквы.
+// Вывести на экран согласные буквы этой строки.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// “hello” => h l l
+// “World” => W r l d
+// “Hello world!” => H l l w r l d*/
+
+void PrintConsonan2(string str)
+{
+    if (str.Length == 0) return;
+    string vowels = "aouyie";
+    if (char.IsAsciiLetter(str[0]) == true && !vowels.Contains(char.ToLower(str[0])))
+    {
+        Console.Write(str[0] + " ");
+    }
+
+    PrintConsonan2(str.Substring(1));
+
+}
+
+
+
+
+
+
+void PrintConsonan(string str, int count)
+{
+    if (count == str.Length) return;
+    string vowels = "aouyie";
+    if (char.IsAsciiLetter(str[count]) == true && !vowels.Contains(char.ToLower(str[count])))
+    {
+        Console.Write(str[count] + " ");
+    }
+
+    PrintConsonan(str, count + 1);
+
+}
+Console.WriteLine("Ввидите строку Латинскими буквами: ");
+string userInput = Console.ReadLine();
+
+PrintConsonan(userInput);
+PrintConsonan2(userInput);
